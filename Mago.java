008dcead41783bd9ccc4b113fa8e5f7a6701a860
ClaudiaCasol <claudiacasolxxi@alumnes.ilerna.com>
2026-02-04 18:44:18 +0100

    Mago hecho

--- Mago.java
+++ Mago.java
@@ -1,3 +1,13 @@
 public class Mago extends Personaje {
-    
+    private int mana;
+    private int manaDisponible;
+
+    public void lanzarHechizo(int costeMana) {
+
+        for (int i = 0; i < costeMana; i++) {
+            if(costeMana > mana) {
+                System.out.println("El hechizo se ha lanzado correctamente.");
+            }
+        }
+    }
 }
